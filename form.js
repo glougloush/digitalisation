4438c4618469d87e4c2919425b881bc010dbcc3d
glougloush <glougloush@gmail.com>
2025-04-20 10:52:21 +0200

    Update form.js

--- form.js
+++ form.js
@@ -15,19 +15,15 @@ document.getElementById("animal-form").addEventListener("submit", function(e) {
     const reponse = avezVous.value;
 
     const nomsOfficiels = [
-        "niet, lulu",
-        "lulu, niet",
-        "niet, petit lulu",
-        "petit lulu, niet"
+        "kawai",
+        "kawaï"
     ];
 
     if (reponse === "non") {
         resultDiv.textContent = "Vous n'avez pas d'animaux!";
     } else if (!combien || isNaN(combien)) {
         resultDiv.textContent = "Indiquez combien d'animaux vous avez.";
-    } else if (combien === 1) {
-        resultDiv.textContent = "Vous êtes sûr d'avoir un seul animal?";
-    } else if (combien !== 2) {
+    } else if (combien !== 1) {
         resultDiv.textContent = "Vous avez trop d'animaux! Le plan gratuit ne permet que de digitaliser 2 animaux!";
     } else if (!nomsOfficiels.includes(noms)) {
         resultDiv.textContent = "Vous devez utiliser le nom officiel de vos animaux!";
